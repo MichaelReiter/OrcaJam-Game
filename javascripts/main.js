76e065fd551ac59a13517a92c8b461cb62f78853
thundoon <thundoon@gmail.com>
2015-09-11 21:42:29 -0700

    add platform flying across

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,31 +17,58 @@ function create() {
   //  A simple background for our game
   game.add.sprite(0, 0, 'sky');
 
-  //  The platforms group contains the ground and the 2 ledges we can jump on
   platforms = game.add.group();
 
-  //  We will enable physics for any object that is created in this group
   platforms.enableBody = true;
 
-  // Here we create the ground.
-  var ground = platforms.create(0, game.world.height - 64, 'ground');
+  game.physics.arcade.enable(platforms);
 
-  //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-  ground.scale.setTo(2, 2);
+  var plat_y_pos = 300;
 
-  //  This stops it from falling away when you jump on it
-  ground.body.immovable = true;
+  var plat_width;
 
-  //  Now let's create two ledges
-  var ledge = platforms.create(400, 400, 'ground');
 
-  ledge.body.immovable = true;
+  plat_y_pos = 100 + ( Math.floor( ( Math.random() * 400 ) + 1 ) );
 
-  ledge = platforms.create(-150, 250, 'ground');
+  plat_width = Math.floor( ( Math.random() * 2 ) + 1 );
 
-  ledge.body.immovable = true;
+  var ledge = platforms.create( 400, plat_y_pos, 'ground' );
+
+  // ledge.scale( plat_width, 1);
+
+  ledge.body.velocity.x = -150;
+
+
+
+  // //  The platforms group contains the ground and the 2 ledges we can jump on
+  // platforms = game.add.group();
+
+  // //  We will enable physics for any object that is created in this group
+  // platforms.enableBody = true;
+
+  // // Here we create the ground.
+  // var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+  // //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+  // ground.scale.setTo(2, 2);
+
+  // //  This stops it from falling away when you jump on it
+  // ground.body.immovable = true;
+
+  // //  Now let's create two ledges
+  // var ledge = platforms.create(400, 400, 'ground');
+
+  // ledge.body.immovable = true;
+
+  // ledge = platforms.create(-150, 250, 'ground');
+
+  // ledge.body.immovable = true;
+
+
 
 }
 
 function update() {
+
+
 }
