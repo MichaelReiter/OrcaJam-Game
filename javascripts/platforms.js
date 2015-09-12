c760216813116bf240d7640fa0c83baf2f9c4186
Michael Reiter <michaelreiter@live.ca>
2015-09-12 10:34:03 -0700

    Add background scaling based on window size

--- javascripts/platforms.js
+++ javascripts/platforms.js
@@ -6,13 +6,9 @@ function createPlatform() {
 
   game.physics.arcade.enable(platforms);
 
-  var plat_y_pos = 300;
+  var plat_y_pos = 100 + ( Math.floor( ( Math.random() * 400 ) + 1 ) );;
 
-  var plat_width;
-
-  plat_y_pos = 100 + ( Math.floor( ( Math.random() * 400 ) + 1 ) );
-
-  plat_width = Math.floor( ( Math.random() * 2 ) + 1 );
+  var plat_width = Math.floor( ( Math.random() * 2 ) + 1 );
 
   var ledge = platforms.create( 400, plat_y_pos, 'ground' );
 
